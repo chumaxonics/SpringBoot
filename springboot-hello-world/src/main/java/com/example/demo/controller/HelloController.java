754c3ee8f5213b71e1b66a0a1eb5ba33ee937bb7
CHUMA GIFT <53385182+chumaxonics@users.noreply.github.com>
2022-07-07 08:28:15 +0200

    Add files via upload

---
+++ springboot-hello-world/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+	/*
+	@GetMapping({"/","/hello"})
+	
+	public String hello(@RequestParam(value = "name",
+	defaultValue = "World", required = true)String name, Model model) {
+		model.addAttribute("name", name);
+		return "hello";
+	}*/
+	@GetMapping({"/","/Login"})
+	public String Login(@RequestParam(value = "username",
+	defaultValue = "Chumaxonics", required = true)String username, Model model) {
+		model.addAttribute("username", username);
+		return "Login";
+	}
+}
